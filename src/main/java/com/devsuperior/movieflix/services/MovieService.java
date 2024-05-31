45ea08a73ffc23799f9aa051eef07aabf0c3e842
Henrique Marques <132225341+henriquecesarjr@users.noreply.github.com>
2024-05-31 10:26:13 -0300

    MovieControlle and MovieService

---
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @Transactional(readOnly = true)
+    public MovieDetailsDTO findById(Long id) {
+        Movie movie = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Filme não encontrado")
+        );
+
+        return new MovieDetailsDTO(movie);
+    }
+
+}
